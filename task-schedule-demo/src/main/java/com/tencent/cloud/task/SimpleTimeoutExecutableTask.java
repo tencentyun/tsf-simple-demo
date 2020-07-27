5d14762240cbd1be7969938ced15c99f45dc4d3b
p_jiangfan <p_jiangfan@tencent.com>
2020-07-27 17:47:23 +0800

    更新 任务调度 demo

--- task-schedule-demo/src/main/java/com/tencent/cloud/task/SimpleTimeoutExecutableTask.java
+++ task-schedule-demo/src/main/java/com/tencent/cloud/task/SimpleTimeoutExecutableTask.java
@@ -1,20 +1,22 @@
 package com.tencent.cloud.task;
 
-import com.tencent.cloud.task.worker.model.ExecutableTaskData;
-import com.tencent.cloud.task.worker.model.ProcessResult;
-import com.tencent.cloud.task.worker.model.TerminateResult;
-import com.tencent.cloud.task.worker.remoting.TaskExecuteFuture;
-import com.tencent.cloud.task.worker.spi.ExecutableTask;
-import com.tencent.cloud.task.worker.spi.TerminableTask;
+import com.tencent.cloud.task.sdk.client.model.ExecutableTaskData;
+import com.tencent.cloud.task.sdk.client.model.ProcessResult;
+import com.tencent.cloud.task.sdk.client.model.TerminateResult;
+import com.tencent.cloud.task.sdk.client.remoting.TaskExecuteFuture;
+import com.tencent.cloud.task.sdk.client.spi.ExecutableTask;
+import com.tencent.cloud.task.sdk.client.spi.TerminableTask;
 import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import java.lang.invoke.MethodHandles;
 
 /**
  * 模拟超时任务, 执行之间在 30s ~ 40s 之间, 可以终止。
  */
+@Component
 public class SimpleTimeoutExecutableTask implements ExecutableTask, TerminableTask {
 
     private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
@@ -24,7 +26,7 @@ public class SimpleTimeoutExecutableTask implements ExecutableTask, TerminableTa
     public ProcessResult execute(ExecutableTaskData taskData) {
         try {
             long startTime = System.currentTimeMillis();
-            long timeOut = RandomUtils.nextLong(30000L, 40000L);
+            long timeOut = RandomUtils.nextLong(30000L, 35000L);
             LOG.info("timeout task start, timeout: {}",timeOut);
             Thread.sleep(timeOut);
             LOG.info("timeout task end, span: {}", System.currentTimeMillis() - startTime);
